cd42e30b8d29908298a4897d4eab751b1acd9ebb
porames-at <porames.at@logicspark.com>
2025-12-19 16:05:37 +0700

    Remove "use client" directive and simplify LazyAppPdfViewer usage in Home component

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,23 +1,15 @@
-"use client";
 import { LazyAppPdfViewer } from "@/components/LazyAppPdfViewer";
-import {useState , useEffect} from "react";
 export default function Home() {
-  const [ready, setReady] = useState(false);
-
-useEffect(() => {
-  const id = setTimeout(() => setReady(true), 2000);
-  return () => clearTimeout(id);
-}, []);
   return (
     <div className="w-[1028px] h-[700px] mx-auto">
       <h1>RP Starter Toolkit: Nextjs + Pages Router + Javascript</h1>
       <br />
       <h2>Default Toolbar</h2>
-      {ready && <LazyAppPdfViewer />}
+      <LazyAppPdfViewer />
       <h2>Without Toolbar</h2>
-      {ready && <LazyAppPdfViewer showToolbar={false} />}
+      <LazyAppPdfViewer showToolbar={false} />
       <h2>Mobile</h2>
-      {ready && <LazyAppPdfViewer defaultLayoutProps={{ style: { width: "500px" } }} />}
+      <LazyAppPdfViewer defaultLayoutProps={{ style: { width: "500px" } }} />
     </div>
   )
 }
