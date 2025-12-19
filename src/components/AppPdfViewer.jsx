b29da2d71c41f22630b126d4c977fb2bbb30a549
porames-at <porames.at@logicspark.com>
2025-12-19 15:53:08 +0700

    Remove unused pdfjs import and disable worker source in AppPdfViewer component

--- src/components/AppPdfViewer.jsx
+++ src/components/AppPdfViewer.jsx
@@ -4,13 +4,9 @@ import {
   RPDefaultLayout,
   RPPages,
 } from "@pdf-viewer/react";
-import { pdfjs } from '@react-pdf/renderer';
-
-
 
 const AppPdfViewer = (props) => {
   const { showToolbar = true, providerProps, defaultLayoutProps } = props;
-  pdfjs.GlobalWorkerOptions.workerSrc = false;
 
   return (
     <RPProvider
