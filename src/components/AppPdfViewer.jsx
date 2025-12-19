3b55a53f9b9c4555de5ec3a432ad7fd0f3f65981
porames-at <porames.at@logicspark.com>
2025-12-19 15:43:58 +0700

    Set pdfjs worker source to false in AppPdfViewer component

--- src/components/AppPdfViewer.jsx
+++ src/components/AppPdfViewer.jsx
@@ -4,9 +4,13 @@ import {
   RPDefaultLayout,
   RPPages,
 } from "@pdf-viewer/react";
+import { pdfjs } from '@react-pdf/renderer';
+
+
 
 const AppPdfViewer = (props) => {
   const { showToolbar = true, providerProps, defaultLayoutProps } = props;
+  pdfjs.GlobalWorkerOptions.workerSrc = false;
 
   return (
     <RPProvider
